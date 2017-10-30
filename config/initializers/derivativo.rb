7f5097bc9fad6e510e034b50e510cc5970e6c9f6
Eric O <eric@ejdigitalmedia.com>
2017-10-30 15:47:29 -0400

    Version 0.1.4; Add ability to override ffmpeg binary path in derivativo.yml

--- config/initializers/derivativo.rb
+++ config/initializers/derivativo.rb
@@ -2,3 +2,6 @@
 Rails.backtrace_cleaner.remove_silencers! if Rails.env.development?
 
 DERIVATIVO = ActiveSupport::HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/derivativo.yml")[Rails.env])
+
+# Override default FFMPEG path from DERIVATIVO config if present
+FFMPEG.ffmpeg_binary = DERIVATIVO['ffmpeg_binary_path'] if DERIVATIVO['ffmpeg_binary_path'].present?

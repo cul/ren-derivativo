7ef9af227854d1663ab3e7fae65e8de0a407383e
Eric O <eric@ejdigitalmedia.com>
2019-03-01 19:11:50 -0500

    Remove outdated comment

--- app/models/concerns/derivativo/pdf_derivatives.rb
+++ app/models/concerns/derivativo/pdf_derivatives.rb
@@ -3,9 +3,7 @@ module Derivativo::PdfDerivatives
 
 	def derivative_proc_for_output_path(out_path)
 		Proc.new do |in_path|
-			# get the path to soffice; this can be nil if soffice is on the $PATH
-			soffice_path = DERIVATIVO['soffice_path']
-			office_convert(in_path, out_path, soffice_path, 'pdf:writer_pdf_Export')
+			office_convert(in_path, out_path, DERIVATIVO['soffice_path'], 'pdf:writer_pdf_Export')
 		end
 	end
 

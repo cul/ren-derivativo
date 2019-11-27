a5c4582a5b6ac7a4da060d9e2d64c260af1cc97b
Eric O <eric@ejdigitalmedia.com>
2019-11-27 11:24:51 -0500

    Version 1.0.1; Update 'processing' deploy target server

--- config/deploy/processing.rb
+++ config/deploy/processing.rb
@@ -1,7 +1,7 @@
 #server "#{fetch(:instance)}-nginx-#{fetch(:stage)}1.cul.columbia.edu", user: fetch(:remote_user), roles: %w(app db web)
 
 # Manual override to dvorak
-server "dvorak.cul.columbia.edu", user: fetch(:remote_user), roles: %w(app db web)
+server "ren-nginx-prod2.cul.columbia.edu", user: fetch(:remote_user), roles: %w(app db web)
 
 # In test/prod, deploy from release tags; most recent version is default
 ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }

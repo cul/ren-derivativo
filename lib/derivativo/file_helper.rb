eb58f7b46c90f9721a0628a0b1a81e0fcd6c2584
Eric O <eric@ejdigitalmedia.com>
2021-04-19 21:03:34 -0400

    Update Derivativo::FileHelper.block_until_file_exists

--- lib/derivativo/file_helper.rb
+++ lib/derivativo/file_helper.rb
@@ -40,9 +40,10 @@ module Derivativo
 
     # Blocks until a file is found at the given path, or until timeout passes
     def self.block_until_file_exists(path_to_file, retry_interval = 0.25, number_of_retries = 3)
+      return true if File.exist?(path_to_file)
       number_of_retries.times do
-        return true if File.exist?(path_to_file)
         sleep retry_interval
+        return true if File.exist?(path_to_file)
       end
       false
     end

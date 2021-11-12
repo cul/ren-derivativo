5019e063a612ba282b9fef0f50184fd854c7ff16
Eric O <eric@ejdigitalmedia.com>
2021-11-12 14:25:33 -0500

    Update deploy environment configurations

---
+++ config/deploy/new_processing.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+server 'ren-nginx-prod2.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
+# In test/prod, suggest latest tag as default version to deploy
+ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }

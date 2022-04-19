0a6f5a6ce66bf4188b07ad086379a8a4b4158d06
Eric O <eric@ejdigitalmedia.com>
2022-04-19 18:23:41 -0400

    In application.rb, set config.load_defaults to 6.0; Remove unused Rails 5.2 framework defaults initializer file

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Derivativo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,7 +21,7 @@ module Derivativo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
+
     # Custom directories with classes and modules you want to be eager loaded.
     config.eager_load_paths += %W[#{config.root}/lib]
 

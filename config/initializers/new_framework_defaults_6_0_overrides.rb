543b3d111852dd99638d6851f8ede82215dc78cf
Eric O <eric@ejdigitalmedia.com>
2022-04-19 18:47:43 -0400

    Disable cache versioning because it's not compatible with our current version of ActiveSupport::Cache::RedisStore

--- config/initializers/new_framework_defaults_6_0_overrides.rb
+++ config/initializers/new_framework_defaults_6_0_overrides.rb
@@ -5,3 +5,8 @@
 # For now, we'll stick with the classic autoloader.
 # This won't be a problem in the next version of Derivativo, which doesn't use the cul_hydra gem.
 Rails.application.config.autoloader = :classic
+
+# Disable cache versioning, which is a Rails 6 feature that's not compatible with the current
+# version of our ActiveSupport::Cache::RedisStore cache (and we're not ready to update to a newer
+# version of ActiveSupport::Cache::RedisStore yet).
+Rails.application.config.active_record.cache_versioning = false

2a7df395fdf51da94a618661710c88cdfb0863cf
Eric O <eric@ejdigitalmedia.com>
2023-09-21 12:24:20 -0400

    Deployment updates

--- config/environments/derivativo_new_processing.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require Rails.root.join('config/environments/deployed.rb')
-
-Rails.application.configure do
-  # Setting host so that url helpers can be used in mailer views.
-  config.action_mailer.default_url_options = { host: 'https://derivativo-new-processing.library.columbia.edu' }
-end

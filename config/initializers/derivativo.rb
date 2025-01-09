5718c8c2fcaed370b04cd265140715ae2b3981cb
Eric O <eric@ejdigitalmedia.com>
2025-01-09 18:39:17 -0500

    Add ability to configure vips_tmp_directory

--- config/initializers/derivativo.rb
+++ config/initializers/derivativo.rb
@@ -9,17 +9,22 @@ DERIVATIVO = Rails.application.config_for(:derivativo).deep_symbolize_keys
 FFMPEG.ffmpeg_binary = DERIVATIVO['ffmpeg_binary_path'] if DERIVATIVO['ffmpeg_binary_path'].present?
 FFMPEG.ffprobe_binary = DERIVATIVO['ffprobe_binary_path'] if DERIVATIVO['ffprobe_binary_path'].present?
 
-# If no working_directory is set, use default tmpdir.
+# If working_directory is not set, default to ruby temp dir
 if DERIVATIVO['working_directory'].blank?
-  DERIVATIVO['working_directory'] = Dir.tmpdir
-else
-  # Create user-specified working directory if it doesn't already exist.
-  FileUtils.mkdir_p(DERIVATIVO['working_directory'])
+  DERIVATIVO['working_directory'] = File.join(Dir.tmpdir, Rails.application.class.module_parent_name.downcase)
 end
+# Make working if it does not already exist
+FileUtils.mkdir_p(DERIVATIVO['working_directory'])
 
-# Set the TMPDIR ENV variable so that Vips (via Imogen) writes temp files here.
+# If vips_tmp_directory is not set, default to ruby temp dir
+if DERIVATIVO['vips_tmp_directory'].blank?
+  DERIVATIVO['vips_tmp_directory'] = File.join(Dir.tmpdir, Rails.application.class.module_parent_name.downcase)
+end
+# Make vips_tmp_directory if it does not already exist
+FileUtils.mkdir_p(DERIVATIVO['vips_tmp_directory'])
+# Set the TMPDIR ENV variable so that Vips (via Imogen) writes temp files to the vips_tmp_directory.
 # This defaults to the OS temp directory if not otherwise set, which can be a
 # problem if we're on a host that has limited local disk space.
-ENV['TMPDIR'] = DERIVATIVO['working_directory']
+ENV['TMPDIR'] = DERIVATIVO['vips_tmp_directory']
 
 Rails.application.config.active_job.queue_adapter = :inline if DERIVATIVO['run_queued_jobs_inline']
